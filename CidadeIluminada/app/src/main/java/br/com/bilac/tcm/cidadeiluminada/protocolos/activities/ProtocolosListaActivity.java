993d8cd9976fb1fc21c453335a00ebdaf0746e52
Arthur Bressan <arthurpbressan@gmail.com>
2015-05-03 16:53:08 -0300

    Lista de protocolos preenchendo protocolos

--- CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/activities/ProtocolosListaActivity.java
+++ CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/activities/ProtocolosListaActivity.java
@@ -1,36 +1,61 @@
 package br.com.bilac.tcm.cidadeiluminada.protocolos.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import br.com.bilac.tcm.cidadeiluminada.R;
+import java.util.ArrayList;
 
-public class ProtocolosListaActivity extends ActionBarActivity {
+import br.com.bilac.tcm.cidadeiluminada.R;
+import br.com.bilac.tcm.cidadeiluminada.models.Protocolo;
+import br.com.bilac.tcm.cidadeiluminada.protocolos.adapters.ProtocoloAdapter;
+
+public class ProtocolosListaActivity extends ListActivity {
+
+    private ProtocoloAdapter protocoloAdapter;
+    private ArrayList<Protocolo> protocolos;
+
+    private Runnable protocolosListRunnable = new Runnable() {
+        @Override
+        public void run() {
+            protocoloListHandler.sendEmptyMessage(0);
+        }
+    };
+
+    private Handler protocoloListHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            protocolos = (ArrayList<Protocolo>) Protocolo.listAll(Protocolo.class);
+            protocoloAdapter = new ProtocoloAdapter(ProtocolosListaActivity.this,
+                    R.layout.protocolo_item_view, protocolos);
+            setListAdapter(protocoloAdapter);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocolos_lista);
+        protocoloAdapter = new ProtocoloAdapter(this, R.layout.protocolo_item_view,
+                new ArrayList<Protocolo>());
+        setListAdapter(protocoloAdapter);
+        new Thread(null, protocolosListRunnable, "FillProtocolosThread").start();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_protocolos_lista, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
