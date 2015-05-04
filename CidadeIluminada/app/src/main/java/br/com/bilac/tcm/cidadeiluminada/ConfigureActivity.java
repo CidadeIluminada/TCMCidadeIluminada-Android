d2ff663c67eb273767eb2d9c356ad85db1e09d81
Rômulo Lima <romulolink@gmail.com>
2015-05-04 14:27:36 -0300

    Funcionando parcialmente

--- CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/ConfigureActivity.java
+++ CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/ConfigureActivity.java
@@ -19,7 +19,7 @@ public class ConfigureActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_configure, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
