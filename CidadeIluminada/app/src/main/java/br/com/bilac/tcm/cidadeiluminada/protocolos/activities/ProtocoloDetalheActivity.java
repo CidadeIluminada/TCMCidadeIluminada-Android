205f2923be1f9a2a382597b94c59484706894d67
Arthur Bressan <arthurpbressan@gmail.com>
2015-05-09 18:55:18 -0300

    Carrega os dados do protocolo

--- CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/activities/ProtocoloDetalheActivity.java
+++ CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/protocolos/activities/ProtocoloDetalheActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
+import br.com.bilac.tcm.cidadeiluminada.Constants;
 import br.com.bilac.tcm.cidadeiluminada.R;
 import br.com.bilac.tcm.cidadeiluminada.activities.SettingsActivity;
+import br.com.bilac.tcm.cidadeiluminada.models.Protocolo;
 
 /**
  * Created by Work on 04/05/2015.
@@ -18,6 +21,30 @@ public class ProtocoloDetalheActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_protocolo_detalhe);
+
+        Intent startupIntent = getIntent();
+        long protocoloId = startupIntent.getLongExtra(Constants.PROTOCOLO_ID_KEY, -1);
+
+        if (protocoloId == -1) {
+            finish();
+        }
+
+        preencherDadosProtocolo(protocoloId);
+    }
+
+    private void preencherDadosProtocolo(long protocoloId) {
+        Protocolo protocolo = Protocolo.findById(Protocolo.class, protocoloId);
+        TextView descricao = (TextView) findViewById(R.id.descricaoProtocoloText);
+        TextView cep = (TextView) findViewById(R.id.cepText);
+        TextView bairro = (TextView) findViewById(R.id.bairroText);
+        TextView rua = (TextView) findViewById(R.id.ruaText);
+        TextView numero = (TextView) findViewById(R.id.numeroText);
+
+        descricao.setText(protocolo.getDescricao());
+        cep.setText(protocolo.getCep());
+        bairro.setText(protocolo.getBairro());
+        rua.setText(protocolo.getLogradouro());
+        numero.setText(protocolo.getNumero());
     }
 
     @Override
