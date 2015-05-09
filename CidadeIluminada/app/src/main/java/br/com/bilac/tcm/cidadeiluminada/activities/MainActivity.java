45ca4bcd2f1687c390d220dcec9b8c23d49a92b6
Arthur Bressan <arthurpbressan@gmail.com>
2015-05-09 17:33:09 -0300

    Adiciona logo da app no menu principal e adiciona scrollview para os botões

--- CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/activities/MainActivity.java
+++ CidadeIluminada/app/src/main/java/br/com/bilac/tcm/cidadeiluminada/activities/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
